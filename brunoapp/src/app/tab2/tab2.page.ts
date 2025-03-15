53d5f45e5b7f4fa1072d4f5c1fad253b693a2bb3
Luiggino <ccluiggino@gmail.com>
2025-03-14 22:19:11 -0600

    Services UI and color theme

--- brunoapp/src/app/tab2/tab2.page.ts
+++ brunoapp/src/app/tab2/tab2.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonSearchbar } from '@ionic/angular/standalone';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,
+    IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSearchbar
+  ]
+
 })
 export class Tab2Page {
 
