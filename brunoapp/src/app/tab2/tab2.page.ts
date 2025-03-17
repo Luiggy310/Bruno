a81e7332ad4f77b74a6eca968a76d3c25b5a2888
Luiggino <ccluiggino@gmail.com>
2025-03-16 18:56:39 -0600

    Minor changes and Logo upload

--- brunoapp/src/app/tab2/tab2.page.ts
+++ brunoapp/src/app/tab2/tab2.page.ts
@@ -3,18 +3,24 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { IonSearchbar } from '@ionic/angular/standalone';
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
+import { IonButton,IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { airplane, carSport, boat, bicycle,
+  calendarClear,settings, call, location, chatbubbleEllipses } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,
-    IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSearchbar
-  ]
-
+    IonSearchbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSearchbar,
+    IonIcon, IonButton
+  ],
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  constructor() {
+    addIcons({ airplane, carSport, boat, bicycle ,
+      calendarClear,settings, call, location, chatbubbleEllipses
+     });
+  }
 }
--- brunoapp/src/app/tab3/tab3.page.ts
+++ brunoapp/src/app/tab3/tab3.page.ts
@@ -2,15 +2,25 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { IonSearchbar } from '@ionic/angular/standalone';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
+import { IonButton,IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { airplane, carSport, boat, bicycle,
+  calendarClear,settings, call, location, chatbubbleEllipses } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,
-    IonSearchbar
+    IonSearchbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSearchbar,
+    IonIcon, IonButton
   ],
 })
 export class Tab3Page {
-  constructor() {}
+  constructor() {
+    addIcons({ airplane, carSport, boat, bicycle ,
+      calendarClear,settings, call, location, chatbubbleEllipses
+     });
+  }
 }
